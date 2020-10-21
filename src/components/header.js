486f8fd425b9c8188554fd7d6162e8f2fe82d75b
Pablo <pm.gonzaalez@gmail.com>
2020-10-21 03:43:02 -0300

    Update header.js
    botonera

--- src/components/header.js
+++ src/components/header.js
@@ -2,6 +2,8 @@ import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import "bootstrap/dist/css/bootstrap.css";
 import logo from "../logo.svg";
+import Button from "react-bootstrap/Button";
+import Nav from "react-bootstrap/Nav";
 
 function Header() {
   return (
@@ -17,6 +19,11 @@ function Header() {
           />{" "}
           SISTELITEN
         </Navbar.Brand>
+        <Nav className="ml-auto text-right">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#features">Features</Nav.Link>
+          <Nav.Link href="#pricing">Pricing</Nav.Link>
+        </Nav>
       </Navbar>
     </>
   );
